e5a7a50d20cbd31269fd8d7ece195b7b7521f5fb
Ian A <>
2018-06-22 09:33:32 +1000

    Adding nav and side bar

--- angular-full-app-with-angular-material/fitness-tracker/src/app/material.module.ts
+++ angular-full-app-with-angular-material/fitness-tracker/src/app/material.module.ts
@@ -6,21 +6,24 @@ import {
   MatFormFieldModule,
   MatIconModule,
   MatInputModule,
-  MatNativeDateModule
+  MatNativeDateModule,
+  MatSidenavModule
 } from '@angular/material';
 
 @NgModule({
   imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatCheckboxModule],
+    MatCheckboxModule,
+    MatSidenavModule],
   exports: [MatButtonModule,
     MatIconModule,
     MatFormFieldModule,
     MatInputModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatCheckboxModule]
+    MatCheckboxModule,
+    MatSidenavModule]
 })
 
 export class MaterialModule {
