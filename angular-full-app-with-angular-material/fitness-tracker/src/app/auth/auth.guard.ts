4a8e946c1da588b449f666a2228e7cb7db95cbfa
Ian A <>
2018-06-30 23:49:27 +1000

    angular-full-app-with-angular-material-angularfire-ngrx: Route Protection

---
+++ angular-full-app-with-angular-material/fitness-tracker/src/app/auth/auth.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/Rx';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService,
+              private router: Router) {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.authService.isAuth()) {
+      return true;
+    }
+    this.router.navigate(['/login'])
+  }
+
+}
