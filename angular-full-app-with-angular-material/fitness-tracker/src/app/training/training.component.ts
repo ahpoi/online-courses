c79966d55039feaed12fbaf6374c1616f0373bf1
Ian A <>
2018-07-10 14:03:02 +1000

    angular-full-app-with-angular-material-angularfire-ngrx: Controlling the Active Exercise + Adding a Form to the Training Component

--- angular-full-app-with-angular-material/fitness-tracker/src/app/training/training.component.ts
+++ angular-full-app-with-angular-material/fitness-tracker/src/app/training/training.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Rx';
+import { TrainingService } from './training.service';
 
 @Component({
   selector: 'app-training',
@@ -8,10 +10,15 @@ import { Component, OnInit } from '@angular/core';
 export class TrainingComponent implements OnInit {
 
   ongoingTraining = false;
+  exerciseSubscription: Subscription;
 
-  constructor() { }
+  constructor(private trainingService: TrainingService) {
+  }
 
   ngOnInit() {
+    this.exerciseSubscription = this.trainingService.exerciseChanged.subscribe(exercise => {
+      this.ongoingTraining = exercise ? true : false;
+    })
   }
 
 }
--- angular-full-app-with-angular-material/fitness-tracker/src/app/training/training.service.ts
+++ angular-full-app-with-angular-material/fitness-tracker/src/app/training/training.service.ts
@@ -1,6 +1,10 @@
+import { Subject } from 'rxjs/Rx';
 import { Exercise } from './exercise.model';
 
 export class TrainingService {
+
+  exerciseChanged = new Subject<Exercise>();
+
   private availableExercise: Exercise[] = [
     {id: 'crunches', name: 'Crunches', duration: 30, calories: 8},
     {id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15},
@@ -8,8 +12,15 @@ export class TrainingService {
     {id: 'burpees', name: 'Burpees', duration: 60, calories: 8}
   ];
 
+  private runningExercise: Exercise;
+
   getAvailableExercises() {
     return this.availableExercise.slice();
+  }
+
+  startExercise(selectedId: string) {
+    this.runningExercise = this.availableExercise.find(ex => ex.id === selectedId);
+    this.exerciseChanged.next({...this.runningExercise})
   }
 
 }
