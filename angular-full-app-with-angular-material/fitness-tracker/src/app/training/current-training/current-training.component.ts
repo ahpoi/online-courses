df4e9470759cbbdc7121164943b9109b3699e433
Ian A <>
2018-07-10 16:35:25 +1000

    angular-full-app-with-angular-material-angularfire-ngrx: Handling complete and cancelled event

--- angular-full-app-with-angular-material/fitness-tracker/src/app/training/current-training/current-training.component.ts
+++ angular-full-app-with-angular-material/fitness-tracker/src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { TrainingService } from '../training.service';
 import { StopTrainingComponent } from './stop-training-component';
@@ -9,8 +9,6 @@ import { StopTrainingComponent } from './stop-training-component';
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
-
-  @Output() trainingExit = new EventEmitter();
 
   progress = 0;
   timer: number;
@@ -28,6 +26,7 @@ export class CurrentTrainingComponent implements OnInit {
     this.timer = setInterval(() => {
       this.progress = this.progress + 5;
       if (this.progress >= 100) {
+        this.trainingService.completeExercise();
         clearInterval(this.timer);
       }
     }, step);
@@ -43,7 +42,7 @@ export class CurrentTrainingComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
-        this.trainingExit.emit();
+        this.trainingService.cancelExercise(this.progress)
       } else {
         this.startOrResumeTimer();
       }
