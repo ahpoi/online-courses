c02a009737b84817f51d66148f5226ec80766d34
Ian A <>
2018-08-05 21:20:25 +1000

    angular-full-app-with-angular-material-angularfire-ngrx: Creating shared module

--- angular-full-app-with-angular-material/fitness-tracker/src/app/auth/auth.module.ts
+++ angular-full-app-with-angular-material/fitness-tracker/src/app/auth/auth.module.ts
@@ -1,9 +1,7 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AngularFireAuthModule } from 'angularfire2/auth';
-import { MaterialModule } from '../material.module';
+import { SharedModule } from '../shared/shared.module';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
@@ -12,12 +10,9 @@ import { SignupComponent } from './signup/signup.component';
     SignupComponent,
     LoginComponent],
   imports: [
-    CommonModule,
-    MaterialModule,
     ReactiveFormsModule,
-    FormsModule,
-    FlexLayoutModule,
     AngularFireAuthModule,
+    SharedModule
   ],
   exports: []
 })
