46daf534562aa2faaf491afe794d112042ea07ae
Ian A <>
2018-08-06 22:40:06 +1000

    angular-full-app-with-angular-material-angularfire-ngrx: Fix prod environment.ts

--- angular-full-app-with-angular-material/fitness-tracker/src/environments/environment.prod.ts
+++ angular-full-app-with-angular-material/fitness-tracker/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBnNpbd7m8TXpfWTDfXxKRfjb-4B81Ojqk",
+    authDomain: "udemy-fitness-tracker-86c34.firebaseapp.com",
+    databaseURL: "https://udemy-fitness-tracker-86c34.firebaseio.com",
+    projectId: "udemy-fitness-tracker-86c34",
+    storageBucket: "udemy-fitness-tracker-86c34.appspot.com",
+    messagingSenderId: "834126946874"
+  }
 };
