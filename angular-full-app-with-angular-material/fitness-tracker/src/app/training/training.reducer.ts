edef206a3686be653ea5bd62fa8b9ee9f20d6a2c
Ian A <>
2018-09-12 16:25:34 +1000

    angular-full-app-with-angular-material-angularfire-ngrx: Lazy load state

---
+++ angular-full-app-with-angular-material/fitness-tracker/src/app/training/training.reducer.ts
@@ -0,0 +1,49 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import * as fromRoot from '../app.reducer'
+import { Exercise } from './exercise.model';
+import {
+  SET_AVAILABLE_TRAININGS,
+  SET_FINISHED_TRAININGS,
+  START_TRAINING,
+  STOP_TRAINING,
+  TrainingActions
+} from './training.actions';
+
+//Since Training module its lazily loaded, we can't add it to the appReducer
+export interface TrainingState {
+  availableExercises: Exercise[];
+  finishedExercises: Exercise[];
+  activeTraining: Exercise;
+}
+
+//Our AppState does not know about the TrainingState but the TrainingState knows about the app state
+//Whenever we load this module lazily, ngRX will merge it behind the scene (Little instruction)
+export interface State extends fromRoot.State {
+  training: TrainingState
+}
+
+const initialState: TrainingState = {
+  availableExercises: [],
+  finishedExercises: [],
+  activeTraining: null
+};
+
+export function trainingReducer(state = initialState, action: TrainingActions): TrainingState {
+  switch (action.type) {
+    case SET_AVAILABLE_TRAININGS:
+      return {...state, availableExercises: action.payload};
+    case SET_FINISHED_TRAININGS:
+      return {...state, finishedExercises: action.payload};
+    case START_TRAINING:
+      return {...state, activeTraining: action.payload};
+    case STOP_TRAINING:
+      return {...state, activeTraining: null};
+    default:
+      return state;
+  }
+}
+
+export const getTrainingState = createFeatureSelector<TrainingState>('training');
+export const getAvailableExercises = createSelector(getTrainingState,(state: TrainingState) => state.availableExercises);
+export const getFinishedExercises = createSelector(getTrainingState,(state: TrainingState) => state.finishedExercises);
+export const getActiveTraining = createSelector(getTrainingState,(state: TrainingState) => state.activeTraining);
